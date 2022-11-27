ba3138a5fd2bc5c7da6aba4f8f1d57bcf2681e46
Soujiro17 <vicente_tomi@hotmail.com>
2022-11-27 12:51:46 -0300

    complicao

--- certamen2/game/game.go
+++ certamen2/game/game.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"fmt"
 	"math"
 	"time"
 
@@ -17,9 +18,9 @@ type Coordinates struct {
 type Options struct {
 	PacmanEffect bool
 	Speed        int
-	Height			 int
-	Width			 	 int
-	Snakes			 int
+	Height       int
+	Width        int
+	Snakes       int
 }
 
 func Play(keyPressed chan termbox.Key, options Options) {
@@ -39,7 +40,7 @@ func Play(keyPressed chan termbox.Key, options Options) {
 	// del score
 
 	// Creamos la serpiente
-	snake := Init(height, width, 5)
+	snake := Init(height, width, 0)
 
 	// Inicializamos una variable Food que generaremos con comida
 	// además de pasarle la altura y la anchura del tablero para
@@ -52,7 +53,15 @@ func Play(keyPressed chan termbox.Key, options Options) {
 	alive := true
 	maxLength := (width - 2) * (height - 2)
 
-	for {
+	mitad := Coordinates{X: int(math.Floor(float64(width/2 - 1))), Y: int(math.Floor(float64(height/2 - 1)))}
+
+	paused := false
+
+	for { // while true iterar las serpientes o aplicar go func() {}
+
+		// var dX float64 = math.Abs(float64(snake.Positions[0].X) - float64(food.Position.X))
+		// var dY float64 = math.Abs(float64(snake.Positions[0].Y) - float64(food.Position.Y))
+
 		if !alive { // Si no estás vivo (o sea, perdiste)
 			str := "Perdiste"
 
@@ -73,70 +82,102 @@ func Play(keyPressed chan termbox.Key, options Options) {
 			DrawString(x-(len(str)/2), y+1, str, termbox.ColorDefault, termbox.ColorDefault)
 			break
 		}
-		select { // Si sigue vivo
-			case key := <-keyPressed: // Tiene que ver con el channel
-				switch key {
-					case termbox.KeyArrowUp:
-						if snake.Direction != DOWN {
-							snake.Direction = UP
-						}
-					case termbox.KeyArrowRight:
-						if snake.Direction != LEFT {
-							snake.Direction = RIGHT
-						}
-					case termbox.KeyArrowDown:
-						if snake.Direction != UP {
-							snake.Direction = DOWN
-						}
-					case termbox.KeyArrowLeft:
-						if snake.Direction != RIGHT {
-							snake.Direction = LEFT
-						}
+
+		select {
+		case key := <-keyPressed:
+			switch key {
+			case termbox.KeyEsc:
+				break
+			case termbox.KeySpace:
+				paused = !paused
+			}
+		default:
+			if paused {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+			str := fmt.Sprintf("Coords snake: (%v, %v)", snake.Positions[0].X, snake.Positions[0].Y)
+			str2 := fmt.Sprintf("Coords food: (%v, %v)", food.Position.X, food.Position.Y)
+			str3 := fmt.Sprintf("Direction: %v", snake.Direction)
+			str6 := fmt.Sprintf("Mitad tablero (%v, %v)", mitad.X, mitad.Y)
+
+			DrawString(width+10, mitad.Y, str, termbox.ColorWhite, termbox.ColorBlack)
+			DrawString(width+10, mitad.Y+1, str2, termbox.ColorWhite, termbox.ColorBlack)
+			DrawString(width+10, mitad.Y+2, str3, termbox.ColorWhite, termbox.ColorBlack)
+
+			DrawString(width+10, mitad.Y+6, str6, termbox.ColorWhite, termbox.ColorBlack)
+
+			// Bordes del tablero
+			DrawString(1, 1, "X", termbox.ColorWhite, termbox.ColorBlack)
+			DrawString(1, height-2, "X", termbox.ColorWhite, termbox.ColorBlack)
+			DrawString(width-2, 1, "X", termbox.ColorWhite, termbox.ColorBlack)
+			DrawString(width-2, height-2, "X", termbox.ColorWhite, termbox.ColorBlack)
+
+			// Cuando se genera un alimento en la misma fila que la serpiente
+			// esta intenta girar por su cola y pierde porque la última posición
+			// de la cola está en el mismo eje Y que la cabeza
+
+			// Ejemplo del porqué pasa: food en (30, 28), cabeza en (30,24)
+			// quiere decir que snake.Positions[0].X == food.Position.X
+			// por lo que entrará en las condicionales de la Y, y en ese
+			// momento la serpiente cambia de dirección y choca con su cola.
+
+			if snake.Positions[0].Y != food.Position.Y && food.Position.Y >= mitad.Y {
+				snake.Direction = DOWN
+			}
+			if snake.Positions[0].Y != food.Position.Y && food.Position.Y < mitad.Y {
+				snake.Direction = UP
+			}
+			if snake.Positions[0].X != food.Position.X && food.Position.X >= mitad.X {
+				snake.Direction = RIGHT
+			}
+			if snake.Positions[0].X != food.Position.X && food.Position.X < mitad.X {
+				snake.Direction = LEFT
+			}
+
+			if snake.FoodEaten { // Si la serpiente se come una fruta
+				snake.Grow() // La serpiente crece
+				alive = snake.Move(false, options)
+				food.Generate(snake, height, width) // Se genera una nueva comida
+
+				if snake.Length == maxLength { // Si alcanza el tamaño máximo
+					str := "Ganaste!"
+
+					// Centro del tablero
+					x := int(math.Floor(float64(width / 2)))
+					y := int(math.Floor(float64(height / 2)))
+
+					// Dibujamos el texto que comience en la posición x-(len(str)/2)
+					// para que sea precisamente en el centro
+					DrawString(x-(len(str)/2), y-1, str, termbox.ColorDefault, termbox.ColorDefault)
+
+					str = "Presiona enter para comenzar un nuevo juego"
+
+					// Lo mismo que el texto de arriba pero 2 líneas
+					// más abajo que el otro
+					x = int(math.Floor(float64(width / 2)))
+					y = int(math.Floor(float64(height/2)) + 2)
+					DrawString(x-(len(str)/2), y+1, str, termbox.ColorDefault, termbox.ColorDefault)
+					break
 				}
-			default:
-				if snake.FoodEaten { // Si la serpiente se come una fruta
-					snake.Grow() // La serpiente crece
-					alive = snake.Move(false, options)
-					food.Generate(snake, height, width) // Se genera una nueva comida
+			} else {
+				alive = snake.Move(true, options)
+			}
 
-					if snake.Length == maxLength { // Si alcanza el tamaño máximo
-						str := "Ganaste!"
+			snake.FoodEaten = false
 
-						// Centro del tablero
-						x := int(math.Floor(float64(width / 2)))
-						y := int(math.Floor(float64(height / 2)))
+			// Si la posición de la fruta es la misma que la de
+			// la serpiente, es porque se la comieron, por lo tanto
+			// foodEaten debe ser true
+			if snake.Positions[0].X == food.Position.X && snake.Positions[0].Y == food.Position.Y {
+				snake.FoodEaten = true
+			}
 
-						// Dibujamos el texto que comience en la posición x-(len(str)/2)
-						// para que sea precisamente en el centro
-						DrawString(x-(len(str)/2), y-1, str, termbox.ColorDefault, termbox.ColorDefault)
-
-						str = "Presiona enter para comenzar un nuevo juego"
-
-						// Lo mismo que el texto de arriba pero 2 líneas
-						// más abajo que el otro
-						x = int(math.Floor(float64(width / 2)))
-						y = int(math.Floor(float64(height/2)) + 2)
-						DrawString(x-(len(str)/2), y+1, str, termbox.ColorDefault, termbox.ColorDefault)
-						break
-					}
-				} else {
-					alive = snake.Move(true, options)
-				}
-
-				snake.FoodEaten = false
-
-				// Si la posición de la fruta es la misma que la de
-				// la serpiente, es porque se la comieron, por lo tanto
-				// foodEaten debe ser true
-				if snake.Positions[0].X == food.Position.X && snake.Positions[0].Y == food.Position.Y {
-					snake.FoodEaten = true
-				}
-
-				// TimeSleep de la velocidad
-				time.Sleep(25 * time.Millisecond * time.Duration(options.Speed))
+			// TimeSleep de la velocidad
+			time.Sleep(25 * time.Millisecond * time.Duration(options.Speed))
 		}
-	}
 
+	}
 	// Si el juego termina, comenzar otro al presionar ENTER
 	for {
 		key := <-keyPressed
--- certamen2/game/player.go
+++ certamen2/game/player.go
@@ -1,6 +1,9 @@
 package game
 
-import "github.com/nsf/termbox-go"
+import (
+	"github.com/nsf/termbox-go"
+	"golang.org/x/exp/slices"
+)
 
 // Direction is the type that reppresents the current direction of the snake
 type Direction uint8
@@ -21,7 +24,59 @@ type Snake struct {
 	Direction Direction
 	Length    int
 	Positions []Coordinates
-	FoodEaten		bool
+	FoodEaten bool
+}
+
+func (s *Snake) SnakeContains(coordenada Coordinates) bool {
+
+	if slices.Contains(s.Positions, coordenada) {
+		return true
+	}
+
+	return false
+}
+
+// Snake is crashing
+func (s *Snake) IsCrashing() (bool, bool, bool, bool) {
+
+	var boolLeft, boolRight, boolUp, boolDown bool
+
+	left := Coordinates{
+		X: s.Positions[0].X - 1,
+		Y: s.Positions[0].Y,
+	}
+
+	right := Coordinates{
+		X: s.Positions[0].X + 1,
+		Y: s.Positions[0].Y,
+	}
+
+	up := Coordinates{
+		X: s.Positions[0].X,
+		Y: s.Positions[0].Y + 1,
+	}
+	down := Coordinates{
+		X: s.Positions[0].X,
+		Y: s.Positions[0].Y - 1,
+	}
+
+	if s.SnakeContains(left) {
+		boolLeft = true
+	}
+
+	if s.SnakeContains(right) {
+		boolRight = true
+	}
+
+	if s.SnakeContains(up) {
+		boolUp = true
+	}
+
+	if s.SnakeContains(down) {
+		boolDown = true
+	}
+
+	return boolLeft, boolRight, boolUp, boolDown
 }
 
 // Move is the function that moves a snake
@@ -34,6 +89,7 @@ func (s *Snake) Move(deleteTail bool, options Options) bool {
 		X: s.Positions[0].X,
 		Y: s.Positions[0].Y,
 	}
+
 	termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('•'), termbox.ColorGreen, termbox.ColorDefault)
 	// Delete tail
 	if deleteTail {
@@ -43,46 +99,47 @@ func (s *Snake) Move(deleteTail bool, options Options) bool {
 	crushedAgainstWall := false
 	// Move head
 	switch s.Direction {
-		case UP:
-			s.Positions[0].Y--
-			if s.Positions[0].Y == 0 {
-				if options.PacmanEffect {
-					s.Positions[0].Y = height - 2
-				} else {
-					crushedAgainstWall = true
-				}
+	case UP:
+		s.Positions[0].Y--
+		if s.Positions[0].Y == 0 {
+			if options.PacmanEffect {
+				s.Positions[0].Y = height - 2
+			} else {
+				crushedAgainstWall = true
 			}
-			termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▴'), termbox.ColorGreen, termbox.ColorDefault)
-		case DOWN:
-			s.Positions[0].Y++
-			if s.Positions[0].Y == height-1 {
-				if options.PacmanEffect {
-					s.Positions[0].Y = 1
-				} else {
-					crushedAgainstWall = true
-				}
+		}
+		// En estas lineas se asignan los colores de la serpiente y la forma
+		termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▴'), termbox.ColorGreen, termbox.ColorDefault)
+	case DOWN:
+		s.Positions[0].Y++
+		if s.Positions[0].Y == height-1 {
+			if options.PacmanEffect {
+				s.Positions[0].Y = 1
+			} else {
+				crushedAgainstWall = true
 			}
-			termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▾'), termbox.ColorGreen, termbox.ColorDefault)
-		case LEFT:
-			s.Positions[0].X--
-			if s.Positions[0].X == 0 {
-				if options.PacmanEffect {
-					s.Positions[0].X = width - 2
-				} else {
-					crushedAgainstWall = true
-				}
+		}
+		termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▾'), termbox.ColorGreen, termbox.ColorDefault)
+	case LEFT:
+		s.Positions[0].X--
+		if s.Positions[0].X == 0 {
+			if options.PacmanEffect {
+				s.Positions[0].X = width - 2
+			} else {
+				crushedAgainstWall = true
 			}
-			termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('◂'), termbox.ColorGreen, termbox.ColorDefault)
-		case RIGHT:
-			s.Positions[0].X++
-			if s.Positions[0].X == width-1 {
-				if options.PacmanEffect {
-					s.Positions[0].X = 1
-				} else {
-					crushedAgainstWall = true
-				}
+		}
+		termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('◂'), termbox.ColorGreen, termbox.ColorDefault)
+	case RIGHT:
+		s.Positions[0].X++
+		if s.Positions[0].X == width-1 {
+			if options.PacmanEffect {
+				s.Positions[0].X = 1
+			} else {
+				crushedAgainstWall = true
 			}
-			termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▸'), termbox.ColorGreen, termbox.ColorDefault)
+		}
+		termbox.SetCell(s.Positions[0].X, s.Positions[0].Y, rune('▸'), termbox.ColorGreen, termbox.ColorDefault)
 
 	}
 	termbox.Flush()
@@ -96,10 +153,12 @@ func (s *Snake) Move(deleteTail bool, options Options) bool {
 		length--
 	}
 	for i := 1; i < s.Length; i++ {
+		// Current position (s.Positions[i]++)
 		partCurrentPosition := Coordinates{
 			X: s.Positions[i].X,
 			Y: s.Positions[i].Y,
 		}
+		// Previous position (s.Positions[i])
 		s.Positions[i].X = previousPosition.X
 		s.Positions[i].Y = previousPosition.Y
 		previousPosition.X = partCurrentPosition.X
@@ -125,6 +184,6 @@ func (s *Snake) Grow() {
 func (s *Snake) Init(height, width, row int) {
 	s.Length = 1
 	s.Positions = append(s.Positions, Coordinates{X: (width / 2) + row, Y: height / 2})
-	termbox.SetCell(s.Positions[0].X + row, s.Positions[0].Y, rune('•'), termbox.ColorGreen, termbox.ColorDefault)
+	termbox.SetCell(s.Positions[0].X+row, s.Positions[0].Y, rune('•'), termbox.ColorGreen, termbox.ColorDefault)
 	termbox.Flush()
 }
